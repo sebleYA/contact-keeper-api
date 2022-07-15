bf9ee40cc6f3d35816ab9ccd3922e3ab9f1ed328
sebli <seblewongelabera@gmail.com>
2022-07-15 10:30:36 -0700

    updated post req for registring new users

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { body, validationResult, check } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public
-
 
 router.post(
   '/',
@@ -19,13 +21,52 @@ router.post(
       'Please enter a password with 6 or more character'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ erros: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Sever Error');
+    }
   }
 );
 
