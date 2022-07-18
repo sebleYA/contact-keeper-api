50ab529c1473e05b7b75bc0149d77971bf1f793d
sebli <seblewongelabera@gmail.com>
2022-07-18 10:28:54 -0700

    corrected the warning causing by Fragmnet classname

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <ContactState>
     <Router>
-      <Fragment className="App">
+      <div className="App">
         <Navbar />
         <div className="container">
           <Switch>
@@ -19,7 +19,7 @@ const App = () => {
             <Route exact path="/about" component={About} />
           </Switch>
         </div>
-      </Fragment>
+      </div>
     </Router>
     </ContactState>
   );
