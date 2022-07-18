0c87437ba06f11b38eaa13e60f7ef1ceecfd9b34
sebli <seblewongelabera@gmail.com>
2022-07-18 10:09:58 -0700

    added methods to delete contacts, set and clear current contact

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactItem = ({ contact }) => {
+  const contactContext = useContext(ContactContext);
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
   const { id, name, email, phone, type } = contact;
+
+  const onDelete = () => {
+    deleteContact(id);
+    clearCurrent();
+  };
+
   return (
-    <div className="card bg-light">
-      <h3 className="text-primary text-left">
+    <div className='card bg-light'>
+      <h3 className='text-primary text-left'>
         {name}{' '}
         <span
           style={{ float: 'right' }}
@@ -17,21 +26,28 @@ const ContactItem = ({ contact }) => {
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
       </h3>
-      <ul className="list">
+      <ul className='list'>
         {email && (
           <li>
-            <i className="fas fa-envelope-open" /> {email}
+            <i className='fas fa-envelope-open' /> {email}
           </li>
         )}
         {phone && (
           <li>
-            <i className="fas fa-phone" /> {phone}
+            <i className='fas fa-phone' /> {phone}
           </li>
         )}
       </ul>
       <p>
-        <button className="btn btn-dark btn-sm">Edit</button>
-        <button className="btn btn-danger btn-sm">Delete</button>
+        <button
+          className='btn btn-dark btn-sm'
+          onClick={() => setCurrent(contact)}
+        >
+          Edit
+        </button>
+        <button className='btn btn-danger btn-sm' onClick={onDelete}>
+          Delete
+        </button>
       </p>
     </div>
   );
